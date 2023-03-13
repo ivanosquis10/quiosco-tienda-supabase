a4cca6161e38d0e899494a98fb578ded88ca2288
ivanosquis10 <83567373+ivanosquis10@users.noreply.github.com>
2023-03-12 21:08:39 -0400

    Se agrego la vista del modal completa

---
+++ components/ModalProducto.js
@@ -0,0 +1,97 @@
+import Image from 'next/image'
+import { formatMoney } from '../helpers'
+import useQuiosco from '../hooks/useQuiosco'
+
+export default function ModalProducto() {
+  const { handleChangeModal, producto } = useQuiosco()
+  const { nombre, imagen, precio } = producto
+
+  return (
+    <div
+      class="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div class="fixed inset-0 bg-slate-900/90 transition-opacity "></div>
+
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div class="relative transform overflow-hidden rounded-lg bg-white shadow-xl transition-all  w-full lg:w-6/12">
+            <div class="bg-white p-6">
+              <div class="flex justify-center gap-4 ">
+                {/*imagen */}
+                <div class="flex flex-shrink-0 items-center justify-center">
+                  <Image
+                    className="rounded-md"
+                    src={`/${imagen}.jpg`}
+                    width={250}
+                    height={400}
+                    alt={`Imagen del platillo ${nombre}`}
+                  />
+                </div>
+
+                {/*boody */}
+                <div class="text-start flex flex-col justify-between">
+                  <div class="mt-2">
+                    <h3
+                      class="text-4xl font-bold text-slate-800"
+                      id="modal-title"
+                    >
+                      {nombre}
+                    </h3>
+                    <p className="text-xl font-bold text-slate-600 mt-1">
+                      Precio:{' '}
+                      <span className="font-bold text-2xl text-slate-800">
+                        {formatMoney(precio)}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="">
+                    <button
+                      type="button"
+                      class="w-full justify-center rounded-md bg-red-600 px-4 py-2 text-xl font-bold text-white shadow-sm hover:bg-red-500"
+                      onClick={() => handleChangeModal()}
+                    >
+                      Cerrar
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+/*
+    <div className="md:flex gap-10 p-10 h-full">
+      <div className="md:w-1/3 border">
+        <Image
+          className="rounded-md"
+          src={`/${imagen}.jpg`}
+          width={250}
+          height={400}
+          alt={`Imagen del platillo ${nombre}`}
+        />
+      </div>
+
+      <div className="md:w-2/3 border">
+        <div className="flex justify-end">
+          <button onClick={() => handleChangeModal()}>cerrar</button>
+        </div>
+
+        <h3 className="text-2xl font-bold text-slate-800 whitespace-pre-wrap tracking-tight mt-2">
+          {nombre}
+        </h3>
+        <p className="text-xl font-bold text-slate-600 mt-1">
+          Precio:{' '}
+          <span className="font-bold text-2xl text-slate-800">
+            {formatMoney(precio)}
+          </span>
+        </p>
+      </div>
+    </div>
+    */
--- layout/Layout.js
+++ layout/Layout.js
@@ -1,7 +1,25 @@
 import Head from 'next/head'
+import Modal from 'react-modal'
 import Sidebar from '../components/Sidebar'
+import ModalProducto from '../components/ModalProducto'
+import useQuiosco from '../hooks/useQuiosco'
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50%',
+  },
+}
+
+Modal.setAppElement('#__next')
 
 export default function Layout({ children, pagina }) {
+  const { modal } = useQuiosco()
   return (
     <>
       <Head>
@@ -17,6 +35,7 @@ export default function Layout({ children, pagina }) {
           <div className="p-10">{children}</div>
         </main>
       </div>
+      {modal && <ModalProducto />}
     </>
   )
 }
